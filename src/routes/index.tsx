0c4909b537c4a4ae53d7219880ccec842b3027f4
Ana Laura <analaurass45@gmail.com>
2024-06-11 14:39:20 -0300

    feat: profile page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,6 +6,7 @@ import Registration from "../pages/registration";
 import styled from "styled-components";
 import { Header } from "../components/Header";
 import ForumAnswer from "../pages/forumAnswer";
+import { Profile } from "../pages/profile";
 
 const Container = styled.main`
   height: 100dvh;
@@ -15,13 +16,14 @@ export const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Container>
-        <Header />
+        <Header logged={true} username="Laura" />
         <Routes>
           <Route path="/" element={<Default />} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
           <Route path="/register" element={<Registration />} />
-          <Route path="/forum-answer" element={<ForumAnswer/>}/>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/forum-answer" element={<ForumAnswer />} />
         </Routes>
       </Container>
     </BrowserRouter>
