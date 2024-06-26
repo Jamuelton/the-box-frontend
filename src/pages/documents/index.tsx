78b3e67a769d282ace1ab2c9c0054d8fa9fa0c05
Jamuelton <jamueltonangelim@gmail.com>
2024-06-25 21:04:54 -0300

    chore: adding button of filter:

--- src/pages/documents/index.tsx
+++ src/pages/documents/index.tsx
@@ -1,5 +1,82 @@
+import { List } from "@phosphor-icons/react";
+import { FilterButton } from "../../components/FilterButton";
+import { OrdenationButton } from "../../components/OrdenationButton";
+import { SearchInput } from "../../components/Search";
+import { Title } from "../../components/Title";
 import * as S from "./styles";
+import { MenuProps } from "antd";
+import { useState } from "react";
 
 export function Documents() {
-  return <S.Container>docs</S.Container>;
+  const [modalFiltro, setModalFiltro] = useState<boolean>(false);
+  const [hamburguer, setHamburguer] = useState<boolean>(false);
+
+  const items: MenuProps["items"] = [
+    {
+      key: 1,
+      label: "Filtros",
+      onClick: () => {
+        setModalFiltro(!modalFiltro);
+      },
+    },
+    {
+      type: "divider",
+    },
+    {
+      key: 2,
+      type: "group",
+      label: "Ordenar",
+      children: [
+        {
+          key: "2-1",
+          label: "Postados Recentemente",
+        },
+        {
+          key: "2-2",
+          label: "Quantidade de Replys",
+        },
+        {
+          key: "2-3",
+          label: "Quantidade de Curtidas",
+        },
+      ],
+    },
+  ];
+  const ordenationItems: MenuProps["items"] = [
+    {
+      key: 1,
+      label: "Postados Recentemente",
+    },
+    {
+      type: "divider",
+    },
+    {
+      key: 2,
+      label: "Quantidade de Replys",
+    },
+    {
+      type: "divider",
+    },
+    {
+      key: 3,
+      label: "Quantidade de Curtidas",
+    },
+  ];
+  return (
+    <S.Container>
+      <Title text={"Documentação do Curso"} />
+      <S.FilterArea>
+        <span>
+          <SearchInput />
+          <S.hamburguerButtons>
+            <OrdenationButton items={ordenationItems} placement="bottomRight" />
+            <FilterButton buttonFunction={() => setModalFiltro(!modalFiltro)} />
+          </S.hamburguerButtons>
+          <S.hamburguerSection menu={{ items }}>
+            <List size={30} onClick={() => setHamburguer(!hamburguer)} />
+          </S.hamburguerSection>
+        </span>
+      </S.FilterArea>
+    </S.Container>
+  );
 }
--- src/pages/documents/styles.ts
+++ src/pages/documents/styles.ts
@@ -1,3 +1,41 @@
+import { Dropdown } from "antd";
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  padding: 1rem;
+`;
+
+export const FilterArea = styled.div`
+  padding: 1rem;
+  display: flex;
+  width: 100%;
+  justify-content: flex-end;
+
+  span {
+    display: flex;
+    width: 50%;
+    gap: 1rem;
+    justify-content: flex-end;
+  }
+`;
+
+export const CardsArea = styled.div``;
+
+export const hamburguerButtons = styled.section`
+  display: flex;
+  flex-direction: row;
+  width: 121%;
+  gap: 1rem;
+
+  @media (max-width: 680px) {
+    display: none;
+  }
+`;
+export const hamburguerSection = styled(Dropdown)`
+  display: none;
+  color: var(--blue-700);
+
+  @media (max-width: 680px) {
+    display: flex;
+  }
+`;
