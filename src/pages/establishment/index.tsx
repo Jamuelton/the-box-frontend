283e7bb2618d723b76043eb364e1be2399fc6dd0
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2024-07-01 23:44:55 -0300

    building establishment page

--- src/pages/establishment/index.tsx
+++ src/pages/establishment/index.tsx
@@ -1,43 +1,142 @@
+import { useParams } from "react-router-dom";
 import { Title } from "../../components/Title";
 import * as S from "./styles";
 
-interface Establishment {
+interface EstablishmentInfo {
   key: number;
   title: string;
   content: string;
   type: string;
-  phone: number;
+  phone: string; // Corrigido para string
   days: string;
-  adress: string;
+  address: string; // Corrigido a grafia
   hours: string;
-  observations: string;
+  observations?: string;
+  socialMedia?: string;
 }
 
-export function Establishment() {
+const cardContent: EstablishmentInfo[] = [
   {
-    /*const cardContent = [
-    {
-      key: 1,
-      title: "Bosco Restaurante",
-      content:
-        "Restaurante aberto de segunda à sabado servindo pratos completos de café da manhã, almoço e janta.",
-      type: "Restaurante",
-      phone: "87 999933805",
-      days: "Seg-Sab",
-      address:
-        "R. Cap. Pedro Rodrigues, 186 - Magano, Garanhuns - PE, 55290-000",
-      hours: "seg até as texta, 07:30–22:00/ sáb, 09:00–19:30",
-      observations:
-        "Preço varia de R$12,00 até R$15,00, podendo fechar combo completo do mes.",
-    },
-  ];*/
+    key: 1,
+    title: "Bosco Restaurante",
+    content:
+      "Restaurante aberto de segunda à sabado servindo pratos completos de café da manhã, almoço e janta.",
+    type: "Restaurante",
+    phone: "87 999933805",
+    days: "Seg-Sab",
+    address: "R. Cap. Pedro Rodrigues, 186 - Magano, Garanhuns - PE, 55290-000",
+    hours: "seg até as texta, 07:30–22:00/ sáb, 09:00–19:30",
+    observations:
+      "Preço varia de R$12,00 até R$15,00, podendo fechar combo completo do mes.",
+  },
+  {
+    key: 2,
+    title: "Oscar Diskgas E Água",
+    content:
+      "Venda e entrega de produtos como botijão de água, gás de cozinha e etc",
+    type: "Serviços Gerais",
+    phone: "87996625532",
+    days: "Segunda à domingo",
+    address: "R. Cap. Pedro Rodrigues, S/N - Magano, Garanhuns - PE, 55294-310",
+    hours: "seg a sab, 07:00-19:30/ domingo, 07:00-14:00",
+  },
+];
+
+export function Establishment() {
+  const { id } = useParams<{ id: string }>();
+  const establishment = cardContent.find(
+    (item) => item.key === parseInt(id || "")
+  );
+
+  if (!establishment) {
+    return <S.Container>Estabelecimento não encontrado</S.Container>;
   }
 
   return (
     <S.Container>
       <S.Title>
-        <Title text={"Estabelecimento"} />
+        <Title text={establishment.title} />
       </S.Title>
+      <S.Wrapper>
+        <S.EstablishmentInfo>
+          <S.SpanLine>
+            <S.ColumnContainer>
+              <S.InfoEstablishmentContainer>
+                <S.InfoEstablishmentTitleText>
+                  Tipo de estabelecimento:
+                </S.InfoEstablishmentTitleText>
+                <S.InfoEstablishmentValueText>
+                  {establishment.type}
+                </S.InfoEstablishmentValueText>
+              </S.InfoEstablishmentContainer>
+              <S.InfoEstablishmentContainer>
+                <S.InfoEstablishmentTitleText>
+                  Telefone:
+                </S.InfoEstablishmentTitleText>
+                <S.InfoEstablishmentValueText>
+                  {establishment?.phone}
+                </S.InfoEstablishmentValueText>
+              </S.InfoEstablishmentContainer>
+              <S.InfoEstablishmentContainer>
+                <S.InfoEstablishmentTitleText>
+                  Horário de funcionamento:
+                </S.InfoEstablishmentTitleText>
+                <S.InfoEstablishmentValueText>
+                  {establishment.hours}
+                </S.InfoEstablishmentValueText>
+              </S.InfoEstablishmentContainer>
+              <S.InfoEstablishmentContainer>
+                <S.InfoEstablishmentTitleText>
+                  Observações:
+                </S.InfoEstablishmentTitleText>
+                <S.InfoEstablishmentValueText>
+                  {establishment?.observations}
+                </S.InfoEstablishmentValueText>
+              </S.InfoEstablishmentContainer>
+            </S.ColumnContainer>
+            <S.ColumnContainer>
+              <S.InfoEstablishmentContainer>
+                <S.InfoEstablishmentTitleText></S.InfoEstablishmentTitleText>
+                <S.InfoEstablishmentValueText></S.InfoEstablishmentValueText>
+              </S.InfoEstablishmentContainer>
+              <S.InfoEstablishmentContainer>
+                <S.InfoEstablishmentTitleText>
+                  Endereço:
+                </S.InfoEstablishmentTitleText>
+                <S.InfoEstablishmentValueText>
+                  {establishment.address}
+                </S.InfoEstablishmentValueText>
+              </S.InfoEstablishmentContainer>
+              <S.InfoEstablishmentContainer>
+                <S.InfoEstablishmentTitleText>
+                  Dias de funcionamento:
+                </S.InfoEstablishmentTitleText>
+                <S.InfoEstablishmentValueText>
+                  {establishment.days}
+                </S.InfoEstablishmentValueText>
+              </S.InfoEstablishmentContainer>
+              <S.InfoEstablishmentContainer>
+                <S.InfoEstablishmentTitleText>
+                  Redes Socias:
+                </S.InfoEstablishmentTitleText>
+                <S.InfoEstablishmentValueText>
+                  {establishment.days}
+                </S.InfoEstablishmentValueText>
+              </S.InfoEstablishmentContainer>
+            </S.ColumnContainer>
+          </S.SpanLine>
+        </S.EstablishmentInfo>
+        <S.EstablishmentGrouped>
+          <S.ImageEstablishmentContainer>
+            <div className="rectangle-blue"></div>
+            <div className="rectangle-grey">Imagem aqui</div>
+          </S.ImageEstablishmentContainer>
+          <S.RateEstablishment/>
+          <S.AvaliationEstablishmentContainer>
+            Ver Avaliação
+          </S.AvaliationEstablishmentContainer>
+        </S.EstablishmentGrouped>
+      </S.Wrapper>
     </S.Container>
   );
 }
--- src/pages/establishment/styles.ts
+++ src/pages/establishment/styles.ts
@@ -5,7 +5,7 @@ export const Container = styled.div`
   flex-direction: column;
   width: 100vw;
   height: 100vh;
-  align-items: center;
+
 `;
 
 export const Title = styled.div`
@@ -13,22 +13,98 @@ export const Title = styled.div`
   padding: 1rem;
 `;
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  display: flex;  
+  flex-direction: row;
+  padding: 4rem 2rem 0 3rem;
+  gap: 6rem;
+`;
 
-export const EstablishmentInfo = styled.div``;
+export const ColumnContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
 
-export const InfoEstablishmentContainer = styled.div``;
+export const SpanLine = styled.span`
+  display: flex;
+  flex-direction: row;
+  gap: 6rem
+`;
 
-export const InfoEstablishmentTitleText = styled.div``;
+export const EstablishmentInfo = styled.section`
+    display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+`;
 
-export const InfoEstablishmentValueText = styled.div``;
+export const InfoEstablishmentContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 3.25rem;
+`;
 
-export const EstablishmentGrouped = styled.div``;
+export const InfoEstablishmentTitleText = styled.div`
+  color: var(--blue-500);
+  font-size: 20px;
+  font-weight: 700;
+  margin-bottom: 5px;
+  `;
 
-export const ImageEstablishmentContainer = styled.div``;
+
+export const InfoEstablishmentValueText = styled.div`
+  font-size: 16px;
+  font-weight: 700;
+  max-width: 400px;
+`;
+
+export const EstablishmentGrouped = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+export const ImageEstablishmentContainer = styled.div`
+  position: relative;
+  display: flex;
+  width: 440px;
+  height: 270px;
+
+  .rectangle-blue {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 440px;
+    height: 260px;
+    background: #7FC7D9;
+    border-radius: 20px;
+  }
+
+  .rectangle-grey {
+    position: absolute;
+    top: 1rem;
+    right: 0.8rem; 
+    width: 440px;
+    height: 262px;
+    background: #DCE2E3;
+    border-radius: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 600;
+    font-size: 20px;
+    color: #000000;
+  }
+`;
 
 export const Img = styled.div``;
 
-export const RateEstablishment = styled.div``;
+export const RateEstablishment = styled.div`
+display: flex;
+`;
 
-export const AvaliationEstablishmentContainer = styled.div``;
+export const AvaliationEstablishmentContainer = styled.div`
+display: flex;
+`;
