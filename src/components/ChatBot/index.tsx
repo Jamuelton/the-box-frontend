726cd41ab4549aadf7641ddcaf40b9033e0f04a9
Jamuelton <jamueltonangelim@gmail.com>
2024-07-08 20:48:04 -0300

    chore: adding chatbot

--- src/components/ChatBot/index.tsx
+++ src/components/ChatBot/index.tsx
@@ -5,9 +5,24 @@ import { Input } from "../Input";
 import { useLocation } from "react-router-dom";
 import { SendMesageBot } from "../../services/ChatServices";
 
+interface Message {
+  sender: string;
+  text: string | Array<{ [key: string]: string }>;
+}
+
 export const ChatBot = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const path = useLocation().pathname;
+
+  const [query, setQuery] = useState<string>("");
+  const [messages, setMessages] = useState<
+    Array<{ sender: string; text: string | Array<Message> }>
+  >([{ sender: "bot", text: "Como poderia te ajudar hoje?" }]);
+
+  const handleChangeQuery = (e: { target: { value: string } }) => {
+    const { value } = e.target;
+    setQuery(value);
+  };
 
   const showModal = () => {
     setIsModalOpen(true);
@@ -22,19 +37,29 @@ export const ChatBot = () => {
   };
 
   const AskBot = async () => {
-    const response = await SendMesageBot("Onde posso almoçar com R$16,00?");
-    if (response?.status == 200) {
-      console.log(response.data);
+    if (query.trim() !== "") {
+      const userMessage = { sender: "user", text: query };
+      setMessages((prevMessages) => [...prevMessages, userMessage]);
+
+      const response = await SendMesageBot(query);
+      if (response?.status === 200) {
+        const responseData = response.data.response;
+        let botMessage;
+
+        if (Array.isArray(responseData)) {
+          botMessage = { sender: "bot", text: responseData };
+        } else {
+          botMessage = { sender: "bot", text: String(responseData) };
+        }
+
+        setMessages((prevMessages) => [...prevMessages, botMessage]);
+      }
+
+      setQuery("");
     }
   };
 
-  //   const answer = [{ label: "Como poderia ajudar hoje?" }];
-
-  //   const question = [
-  //     { label: "Quais os horários do laborátorio disponivel hoje?" },
-  //   ];
-
-  if (path == "/login" || path == "/register") {
+  if (path === "/login" || path === "/register") {
     return null;
   }
 
@@ -51,18 +76,35 @@ export const ChatBot = () => {
         onCancel={handleCancel}
         footer={
           <>
-            <Input placeHolder="Pergunte algo" />
+            <Input
+              placeHolder="Pergunte algo"
+              inputFunction={handleChangeQuery}
+              value={query}
+            />
             <PaperPlaneTilt size={24} weight="fill" onClick={AskBot} />
           </>
         }
       >
-        <S.ChatLabel>
-          <p>Como poderia te ajudar hoje?</p>
-        </S.ChatLabel>
-        <S.userLabel>
-          <label htmlFor="">Você</label>
-          <p>Quais os horários do laborátorio disponivel hoje?</p>
-        </S.userLabel>
+        <S.ChatContent>
+          {messages.map((message, index) => (
+            <S.Message key={index} sender={message.sender}>
+              <label>{message.sender === "bot" ? "Bot" : "Você"}</label>
+              {Array.isArray(message.text) ? (
+                message.text.map((item, subIndex) => (
+                  <div key={subIndex}>
+                    {Object.entries(item).map(([key, value]) => (
+                      <p key={key}>
+                        <strong>{key}:</strong> {value}
+                      </p>
+                    ))}
+                  </div>
+                ))
+              ) : (
+                <p>{message.text}</p>
+              )}
+            </S.Message>
+          ))}
+        </S.ChatContent>
       </S.Chat>
     </S.Container>
   );
